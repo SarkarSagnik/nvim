2d6f0caff3be6ef6a1efc910598f5b503c70c185
Sagnik Sarkar <sagnik.sarkar.2002@outlook.com>
2023-12-17 21:22:31 +0530

    Fresh new nvim config with Lazy.nvim

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,6 @@
+require("core.plugin_config.colors")
+require("core.plugin_config.harpoon")
+require("core.plugin_config.lsp")
+require("core.plugin_config.telescope")
+require("core.plugin_config.undotree")
+require("core.plugin_config.nvimtree_config")
---
+++ lua/core/plugins.lua
@@ -0,0 +1,45 @@
+require("lazy").setup({
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.4",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+
+    {
+        'rose-pine/neovim' ,
+        as = 'rose-pine' ,
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
+
+
+    'theprimeagen/harpoon',
+    'mbbill/undotree',
+    'tpope/vim-vinegar', 
+    'nvim-tree/nvim-tree.lua',
+
+    -- completion
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+})
+
