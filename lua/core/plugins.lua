4db55fb6a57a8288fdd63d51b0537bfe98042998
Sagnik Sarkar <sagnik.sarkar.2002@outlook.com>
2024-01-05 23:35:49 +0530

    Treesitter Updates

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -16,6 +16,23 @@ require("lazy").setup({
     'theprimeagen/harpoon',
     'mbbill/undotree',
 
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        event = 'BufRead',
+        config = function()
+            require'nvim-treesitter.configs'.setup{
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                ensure_installed = { "c", "lua", "vim", "rust", "python", "typescript" },
+
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end
+    },
+
     -- completion
     {
         'VonHeikemen/lsp-zero.nvim',
