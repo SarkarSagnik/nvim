879b9c3433af8b9e1994359a657a9fa744f05a72
SarkarSagnik <sagnik.sarkar.2002@outlook.com>
2025-08-13 10:45:14 +0530

    Everforest theme added

--- lua/core/plugin_config/colors.lua
+++ lua/core/plugin_config/colors.lua
@@ -1,14 +1,11 @@
-require('rose-pine').setup({
-    disable_background = true
-})
-
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
-
+-- Function to set up the color scheme and remove background highlights
+function ColorMyPencils(color)
+  color = color or "everforest"
+  vim.cmd.colorscheme(color)
+  -- Remove background highlight for Normal and NormalFloat
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 end
 
+-- Call the function to apply the color scheme
 ColorMyPencils()
