ea1333f762d4b287fd2bdd1fe43edb198e4c75f8
yhwach <sarkar.sagnik.2002@outlook.com>
2025-11-09 19:48:11 +0530

    updates

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -35,7 +35,7 @@ cmp.setup(lsp_zero.defaults.cmp_config({
     mapping = {
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
     },
     sources = {
